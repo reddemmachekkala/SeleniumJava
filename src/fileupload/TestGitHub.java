fa7ffa73f32cc3d7c2f0b9aae11d3afba24c17e6
reddemmachekkala <reddemma.chekkala@gmail.com>
2025-06-12 17:48:39 +0530

    New Test class added in fileupload package

---
+++ src/fileupload/TestGitHub.java
@@ -0,0 +1,14 @@
+package fileupload;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+public class TestGitHub {
+  @Test
+  public void facebook() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
