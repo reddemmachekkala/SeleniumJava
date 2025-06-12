a371a0653872cdc13d7a1ae4df35263b7e0bac30
reddemmachekkala <reddemma.chekkala@gmail.com>
2025-06-12 18:27:13 +0530

    Update GitHubDemo By Purushotham.java

--- src/dropdowns/GitHubDemo.java
+++ src/dropdowns/GitHubDemo.java
@@ -3,14 +3,20 @@ package dropdowns;
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class GitHubDemo {
 	WebDriver driver;
   @Test
-  public void test() {
+  public void googlesearch() {
+	  driver.get("https://www.google.com");
+	  driver.findElement(By.name("q")).sendKeys("Test GitHub");
   }
   @BeforeTest
   public void beforeTest() {
+	   driver = new ChromeDriver();
+	  driver.manage().window().maximize();
   }
 
 }
