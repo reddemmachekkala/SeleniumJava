94c4ea41a2567000c28ef83764b1b1fe9d72fd70
reddemmachekkala <reddemma.chekkala@gmail.com>
2025-06-12 18:24:11 +0530

    New class added in dropdown

--- src/dropdowns/GitHubDemo.java
+++ src/dropdowns/GitHubDemo.java
@@ -1,11 +1,13 @@
 package dropdowns;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeTest;
 
 public class GitHubDemo {
+	WebDriver driver;
   @Test
-  public void f() {
+  public void test() {
   }
   @BeforeTest
   public void beforeTest() {
