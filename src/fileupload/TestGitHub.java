e8dcb456520a509440e20ff03f47cfbeeeb73350
reddemmachekkala <reddemma.chekkala@gmail.com>
2025-06-12 18:00:15 +0530

    Test

--- src/fileupload/TestGitHub.java
+++ src/fileupload/TestGitHub.java
@@ -1,14 +1,22 @@
 package fileupload;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 
 public class TestGitHub {
-  @Test
-  public void facebook() {
-  }
-  @BeforeTest
-  public void beforeTest() {
-  }
+	WebDriver driver;
+
+	@Test
+	public void techlearn() {
+		driver.get("https://www.techlearn.in");
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
 
 }
